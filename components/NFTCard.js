d7e529b4f8a7a9ac7e3f5eb23806e0a5ee57346b
DevelopJKong <ybaram40@gmail.com>
2022-08-14 16:47:20 +0900

    EndDate 구성중 [1:05:31 coding...]

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -3,6 +3,9 @@ import { useNavigation } from "@react-navigation/native";
 //여기서 native 하고 core의 useNavigation의 차이점은 뭐지?
 
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { CircleButton, RectButton } from "./Button";
+
+import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -22,8 +25,11 @@ const NFTCard = ({ data }) => {
           resizeMode="cover"
           style={{ width: "100%", height: "100%", borderTopLeftRadius: SIZES.font, borderTopLeftRadius: SIZES.font }}
         />
+
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>NFT Card</Text>
+
+      <SubInfo/>
     </View>
   );
 };
